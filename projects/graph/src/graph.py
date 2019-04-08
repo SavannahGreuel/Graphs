2baf6724781f32994ca7f26f74f3d86453feb859
Savannah Greuel <smallidgesavannah@gmail.com>
2019-04-08 13:43:27 -0600

    part 1 - 3.5 implemented

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,101 @@
 """
 Simple graph implementation
 """
+class Queue():
+    def __init__(self):
+        self.queue = []
 
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.queue))
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.verticies = {}
+
+    def add_vertex(self, vertex_id):
+        self.verticies[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.verticies and v2 in self.verticies:
+            self.verticies[v1].add(v2)
+        else: 
+            raise IndexError("that vertex does not exist")
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.verticies and v2 in self.verticies:
+            self.verticies[v1].add(v2)
+            self.verticies[v2].add(v1)
+        else: 
+            raise IndexError("that vertex does not exist")
+    
+    def bft(self, starting_vertex_id):
+        q = Queue()
+        visited = set()
+        q.enqueue(starting_vertex_id)
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.verticies[v]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex_id)
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.verticies[v]:
+                    s.push(neighbor)
+
+    # TODO depfth first traversal using recursion
+    def dft_r(self,starting_vertex_id, visited = None):
+        if visited is None:
+            visited = set()
+        # marks the visited index as visited
+        visited.add(starting_vertex_id)
+        # call dft_r on the unvisited neighbors
+        for neighbor in self.verticies[starting_vertex_id]:
+            if neighbor not in visited:
+                self.dft_r(neighbor,visited)
+
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.verticies)
