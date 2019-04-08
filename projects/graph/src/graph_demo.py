a89b4836de6eb2c5d573f298383150f0c72665e1
Savannah Greuel <smallidgesavannah@gmail.com>
2019-04-08 14:50:44 -0600

    corrected spelling error

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -28,7 +28,7 @@ def main():
     graph.add_directed_edge('3', '5')
     graph.add_directed_edge('2', '3')
     graph.add_directed_edge('4', '6')
-    print(graph.vertices)
+    print(graph.verticies)
 
 if __name__ == '__main__':
     # TODO - parse argv
